4fe5b3720f67f8315a1898598d5ad1c4105a90b7
Emad Ali <mc.lilzee@hotmail.com>
2022-10-22 09:51:37 +0200

    Add Student printing of scores

--- src/main/java/project/Student.java
+++ src/main/java/project/Student.java
@@ -6,11 +6,13 @@ public class Student {
     private String firstName;
     private String lastName;
     private String email;
+    private Course[] courses;
 
     public Student(String firstName, String lastName, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
+        this.courses = new Course[]{new Course("Java"), new Course("DSA"), new Course("Databases"), new Course("Spring")};
     }
 
     public String getFirstName() {
@@ -35,6 +37,30 @@ public class Student {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public void addScores(String[] scores) {
+        if (scores.length < courses.length) {
+            throw new IllegalArgumentException();
+        }
+
+        for (int i = 0; i < scores.length; i++) {
+            int score = Integer.parseInt(scores[i]);
+            courses[i].addScore(score);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder(hashCode()).append(" points: ");
+        for (int i = 0; i < courses.length; i++) {
+            builder.append(courses[i].getName()).append("=").append(courses[i].getScore());
+            if (i != courses.length - 1) {
+                builder.append("; ");
+            }
+        }
+
+        return builder.toString();
     }
 
     @Override
