838c9ff23297eac024b27822379a59803c753005
Emad Ali <mc.lilzee@hotmail.com>
2022-10-23 16:09:48 +0200

    Refactor controller to use dynamic sizes

--- src/main/java/project/statistics/CoursesController.java
+++ src/main/java/project/statistics/CoursesController.java
@@ -41,37 +41,37 @@ class CoursesController {
     List<Course> getMostPopularCourses() {
         int mostPopularCount = Arrays.stream(courses).mapToInt(course -> course.getStudents().size()).max().orElse(-1);
         List<Course> enrolled = Arrays.stream(courses).filter(course -> course.getStudents().size() == mostPopularCount).collect(Collectors.toList());
-        return enrolled.size() != 4 ? enrolled : List.of();
+        return enrolled.size() != Courses.values().length ? enrolled : List.of();
     }
 
     List<Course> getLeastPopularCourses() {
         int leastPopularCount = Arrays.stream(courses).mapToInt(course -> course.getStudents().size()).min().orElse(-1);
         List<Course> enrolled = Arrays.stream(courses).filter(course -> course.getStudents().size() == leastPopularCount).collect(Collectors.toList());
-        return enrolled.size() != 4 ? enrolled : List.of();
+        return enrolled.size() != Courses.values().length ? enrolled : List.of();
     }
 
     List<Course> getHighestActivityCourses() {
         int highestActivityCount = Arrays.stream(courses).mapToInt(Course::getCompletedTasks).max().orElse(-1);
         List<Course> activity = Arrays.stream(courses).filter(course -> course.getCompletedTasks() == highestActivityCount).collect(Collectors.toList());
-        return activity.size() != 4 ? activity : List.of();
+        return activity.size() != Courses.values().length ? activity : List.of();
     }
 
     List<Course> getLowestActivityCourses() {
         int lowestActivityCount = Arrays.stream(courses).mapToInt(Course::getCompletedTasks).min().orElse(-1);
         List<Course> activity = Arrays.stream(courses).filter(course -> course.getCompletedTasks() == lowestActivityCount).collect(Collectors.toList());
-        return activity.size() != 4 ? activity : List.of();
+        return activity.size() != Courses.values().length ? activity : List.of();
     }
 
     List<Course> getEasiestCourses() {
         double highestAverage = Arrays.stream(courses).mapToDouble(Course::getAverageScores).max().orElse(-1);
         List<Course> average = Arrays.stream(courses).filter(course -> course.getAverageScores() == highestAverage).collect(Collectors.toList());
-        return average.size() != 4 ? average : List.of();
+        return average.size() != Courses.values().length ? average : List.of();
     }
 
     List<Course> getHardestCourses() {
         double lowestAverage = Arrays.stream(courses).mapToDouble(Course::getAverageScores).min().orElse(-1);
         List<Course> average = Arrays.stream(courses).filter(course -> course.getAverageScores() == lowestAverage).collect(Collectors.toList());
-        return average.size() != 4 ? average : List.of();
+        return average.size() != Courses.values().length ? average : List.of();
     }
 
     enum Courses {
